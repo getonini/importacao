584e3ee527bc80b353561478835fd874d0be28c5
Guilherme Tonini <guilhermeetonini@gmail.com>
2018-11-02 12:20:52 -0300

    Criacao de projeto Docker e projeto java, para integracao com ElasticSearch.

---
+++ importElastic/src/main/java/com/tonini/jaxRSActiv/JaxRsActivator.java
@@ -0,0 +1,13 @@
+package com.tonini.jaxRSActiv;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+ 
+@ApplicationPath("")
+public class JaxRsActivator extends Application {
+
+    public JaxRsActivator() {
+
+    }
+ 
+}
